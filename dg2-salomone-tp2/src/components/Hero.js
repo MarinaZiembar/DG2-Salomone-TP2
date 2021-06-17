5507ca53f2630ef17be2a8662b5fa546a7e734ee
Marina Ziembar <marinaziembar@gmail.com>
2021-06-17 16:55:05 -0300

    Ajustes vista articulos

--- dg2-salomone-tp2/src/components/Hero.js
+++ dg2-salomone-tp2/src/components/Hero.js
@@ -10,11 +10,10 @@ function Hero() {
   return (
     <div className="Hero">
       <div id="background-hero">
-        <iframe width="200%" height="100%" src="https://www.youtube.com/embed/0k23DVv_xsA?controls=0&autoplay=1&showinfo=0&mute=1&hd=1&rel=0" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; loop" allowFullScreen></iframe>
+        <Link className="brand" to="/">
+          <p>Bang.</p>
+        </Link>
       </div>
-      <Link className="brand" to="/">
-        <p>Bang.</p>
-      </Link>
       <Menu 
          background_url={videoArticulos} 
          dato_curioso="La superficie de la luna está hecha de un polvo pegajoso y sumamente abrasivo, dañino para la respiración y para el vestuario de los astronautas."
--- dg2-salomone-tp2/src/pages/Articulos.js
+++ dg2-salomone-tp2/src/pages/Articulos.js
@@ -10,12 +10,12 @@ function Articulos() {
       <Hero />
       <div className="general-row">
         <div className="col-12">
-          <Link to="/articulo-2">
+          <Link to="/articulo-1">
             <div className="card-articulo-1">
-              <p>Medioambiente</p>
+              <p>Astronomía</p>
               <div className="content">
-                <h2>Cambio<br></br> climático</h2>
-                <h3>5 razones por las que 2021 puede ser un año crucial en la lucha contra el cambio climático.</h3>
+                <h2>Ojos y oídos en el espacio</h2>
+                <h3>Después de un cuarto de siglo de espera, por fin el 31 de octubre veremos despegar el sucesor del telescopio espacial Hubble.</h3>
               </div>
             </div>
           </Link>
@@ -23,11 +23,11 @@ function Articulos() {
       </div>
       <div className="general-row">
         <div className="col-6">
-          <Link to="/articulo-1">
+          <Link to="/articulo-2">
             <div className="card-articulo-2">
-              <p>Astronomía</p>
-                <h2>Ojos y oídos en el espacio</h2>
-                <h4>Después de un cuarto de siglo de espera, por fin el 31 de octubre veremos despegar el sucesor del telescopio espacial Hubble.</h4>
+              <p>Medioambiente</p>
+                <h2>Cambio climático</h2>
+                <h4>5 razones por las que 2021 puede ser un año crucial en la lucha contra el cambio climático.</h4>
             </div>
           </Link>
         </div>
@@ -43,45 +43,45 @@ function Articulos() {
         <div className="col-3">
           <Link to="/articulo-3">
             <div className="card-articulo-4">
-              <h3>El año de las vacunas</h3>
+              <h3>El año de las <span className="enlarge-2">vacunas</span></h3>
             </div>
           </Link>
         </div>
         <div className="col-3">
           <div className="card-articulo-4">
-            <h3>Anne Mclaren la mamá de la fertilización in vitro</h3>
+            <h3>Anne Mclaren la mamá de la <span className="enlarge-1">fertilización</span> in vitro</h3>
           </div>
         </div>
         <div className="col-3">
           <div className="card-articulo-4">
-            <h3>Lucy y Ardi, los fósiles que cambiaron la historia de la humanidad</h3>
+            <h3>Lucy y Ardi, los <span className="enlarge-1">fósiles</span> que cambiaron la historia de la <span className="enlarge-2">humanidad</span></h3>
           </div>
         </div>
         <div className="col-3">
           <div className="card-articulo-4">
-            <h3>DART: Desviaremos un asteroide golpeandolo con una nave.</h3>
+            <h3><span className="enlarge-2">DART:</span> Desviaremos un asteroide <span className="enlarge-1">golpeandolo</span> con una nave.</h3>
           </div>
         </div>
       </div>
       <div className="general-row-2">
         <div className="col-3">
           <div className="card-articulo-4">
-            <h3>Ingenuity en Marte: el exitoso primer vuelo sobre el planeta rojo</h3>
+            <h3>Ingenuity en <span className="enlarge-2">Marte:</span> el exitoso primer vuelo sobre el planeta <span className="enlarge-1">rojo</span></h3>
           </div>
         </div>
         <div className="col-3">
           <div className="card-articulo-4">
-            <h3>Run 3: El LHC del CERN vuelve a acelerar partículas</h3>
+            <h3>Run 3: El LHC del CERN vuelve a <span className="enlarge-2">acelerar</span> partículas</h3>
           </div>
         </div>
         <div className="col-3">
           <div className="card-articulo-4">
-            <h3>Una iniciativa para producir harina de porotos y formular nuevos alimentos</h3>
+            <h3>Una iniciativa para producir harina de porotos y <span className="enlarge-1">formular</span> nuevos alimentos</h3>
           </div>
         </div>
         <div className="col-3">
           <div className="card-articulo-4">
-            <h3>Así es Grace, el robot sanitario creado por el Covid-19</h3>
+            <h3>Así es <span className="enlarge-1">Grace</span>, el robot <span className="enlarge-2">sanitario</span> creado por el Covid-19</h3>
           </div>
         </div>
       </div>
